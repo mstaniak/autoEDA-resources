7fa62b3420b2012b3fcdd127f7885ae9450720e8
Mateusz Staniak <mtst@mstaniak.pl>
2019-03-26 15:01:29 +0100

    updates commits number in the table

--- autoEDA-paper/get_repos_information.R
+++ autoEDA-paper/get_repos_information.R
@@ -35,6 +35,7 @@ stars <- lapply(
 )
 # asave(stars, ".")
 # "2dbb878d8b8ed530395c72e16f250060"
+# stars <- aread("2dbb878d8b8ed530395c72e16f250060")
 ### Number of forks
 forks <- lapply(
     1:length(pkgs),
@@ -45,11 +46,12 @@ forks <- lapply(
   )
 # asave(forks, ".")
 # "f9c5c973db0435f9cc33c771ce0eeb72"
+# forks <- aread("f9c5c973db0435f9cc33c771ce0eeb72")
 ### Number of commits
 commits <- lapply(
   1:length(pkgs),
   function(x)
-      unlist(lapply(1:6, function(y) content(GET(
+      unlist(lapply(1:9, function(y) content(GET(
         url = paste0("https://api.github.com/repos/", users[[x]], "/", pkgs[[x]],
                      "/commits?per_page=100&page=", y,
                      "&access_token=ba1355603134b281bf18295bcb39ac4d7211fcc8"))
@@ -57,7 +59,8 @@ commits <- lapply(
       recursive = FALSE)
 )
 # asave(commits, ".")
-# "5ccbfed0300dd1225378d88911008319"
+# "fc607fdc773ff48287bcea8ba5d574bf"
+# commits <- aread("fc607fdc773ff48287bcea8ba5d574bf")
 ### Number of contributors
 contributors <- lapply(
   1:length(pkgs),
@@ -68,6 +71,7 @@ contributors <- lapply(
 )
 # asave(contributors, ".")
 # "4b5d35082facfa8ea6157040cbc980e6"
+# contributors <- aread("4b5d35082facfa8ea6157040cbc980e6")
 ### Number of issues
 issues <- lapply(
   1:length(pkgs),
@@ -80,6 +84,7 @@ issues <- lapply(
 )
 # asave(issues, ".")
 # "fc51661342be71242018b3f10687df06"
+# issues <- aread("fc51661342be71242018b3f10687df06")
 ## Download data from CRAN ----
 ### Number of CRAN downloads
 cran_downloads_pkgs <- cran_downloads(
@@ -89,6 +94,7 @@ cran_downloads_pkgs <- cran_downloads(
 )
 # asave(cran_downloads_pkgs, ".")
 # "52ec582b95f35297596d1bc1138b1f79"
+# cran_downloads_pkgs <- aread("52ec582b95f35297596d1bc1138b1f79")
 ggplot(filter(cran_downloads_pkgs, count > 0),
        aes(x = date, y = count, color = package)) +
   theme_bw() +
@@ -127,7 +133,7 @@ pkgs_table <- cran_downloads_pkgs %>%
   mutate(release_date = as.character(release_date)) %>%
   mutate(release_date = ifelse(package == "autoEDA", "-", release_date))
 # asave(pkgs_table, ".")
-# "006df76daeb59394b9179624b188e1f5"
+# "bdc8544ff612c86a092eee86cb1ed111"
 pkgs_table %>%
   rename(`CRAN downloads` = all_downloads,
          `CRAN release` = release_date) %>%
